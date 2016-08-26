7da1e8a3c9771cfecb0dc3158c6176e09efbbd2d
SZ <luminasce@gmail.com>
2016-08-26 17:35:24 +0800

    Person model data validation

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,10 @@
 class Person < ApplicationRecord
+  validates: firstName, presence: true, length: { in: 2..20 }
+  validates: lastName, presence: true, length: { in: 2..20 }
+  validates: emailAddress, presence: true, length: { in: 2..20 }
+  validates: telNo, presence: true, numericality: { only_integer: true }, length: { minimum: 7 }
+  validates: ifSubscribed, presence: true
+  validates: gender, presence: true, inclusion: { in: %w(M F), message: "%{value} is not valid" }
   has_many :volunteers
   has_many :organisers
   has_many :attendees
