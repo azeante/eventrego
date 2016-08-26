c350b7b8591a6436605185af40e298c3bc91ecf6
SZ <luminasce@gmail.com>
2016-08-26 17:55:53 +0800

    Person Event model validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,16 @@
 class Event < ApplicationRecord
+  validates: dateOfEvent
+  validates: timeOfEvent
+  validates: venue
+  validates: price
+  validates: title
+  validates: maximumParticipants
+  validates: minimumParticipants
+  validates: participantsMustBring
+  validates: notes
   has_many :volunteers
   has_many :organisers
   has_many :attendees
   has_many :presenters
+
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,8 @@
 class Person < ApplicationRecord
   validates: firstName, presence: true, length: { in: 2..20 }
   validates: lastName, presence: true, length: { in: 2..20 }
-  validates: emailAddress, presence: true, length: { in: 2..20 }
-  validates: telNo, presence: true, numericality: { only_integer: true }, length: { minimum: 7 }
+  validates: emailAddress, presence: true, length: { in: 2..20 }, uniqueness: true
+  validates: telNo, presence: true, numericality: { only_integer: true }, length: { minimum: 7 }, uniqueness: true
   validates: ifSubscribed, presence: true
   validates: gender, presence: true, inclusion: { in: %w(M F), message: "%{value} is not valid" }
   has_many :volunteers
