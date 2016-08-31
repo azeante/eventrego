f3fe66712f0c3fc9a6871c2e8b0862f1f2e0bc22
SZ <luminasce@gmail.com>
2016-08-31 15:29:41 +0800

    attendee test

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,6 +1,17 @@
 class Attendee < ApplicationRecord
-  validates :amountBilled, numericality: true
-  validates :amountPaid, numericality: true
+  validates :amountBilled, numericality: true, unless: :amount_is_under_zero?
+  validates :amountPaid, numericality: true, unless: :amount_is_under_zero?
   belongs_to :person
   belongs_to :event
+
+  def amount_is_under_zero?
+    if amountBilled < 0 || amountPaid < 0
+      return true
+    else
+      return false
+      end
+  end
+
+
+
 end
--- test/models/attendee_test.rb
+++ test/models/attendee_test.rb
@@ -4,4 +4,28 @@ class AttendeeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @attendee = Attendee.new(timePayment: Time.new(2016, 8, 26,  16,  0,  0), amountBilled: 15.00, amountPaid: 0, person_id: 1, event_id: 1)
+  end
+
+  test "should be valid" do
+    assert @attendee.valid?
+  end
+
+  test "amountBilled should be numeric" do
+    @attendee.amountBilled = "aaaa"
+    assert_not @attendee.valid?
+  end
+
+  test "amountPaid should be numeric" do
+    @attendee.amountPaid = "aaaa"
+    assert_not @attendee.valid?
+  end
+
+  test "amount should be at least 0" do
+    @attendee.amountBilled = 1
+    @attendee.amountPaid = 1
+    assert @attendee.valid?
+  end
 end
