26e683312fea07f2c5b0f41cbe0da0fe9f189e81
SZ <luminasce@gmail.com>
2016-08-31 17:14:22 +0800

    volunteer test

--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -4,4 +4,18 @@ class VolunteerTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @volunteer = Volunteer.new(role: "usher", person_id: 1, event_id: 1)
+  end
+
+  test "should be valid" do
+    assert @volunteer.valid?
+  end
+
+  test "role should not be too long" do
+    @volunteer.role = "a" * 141
+    assert_not @volunteer.valid?
+  end
+
+
 end
