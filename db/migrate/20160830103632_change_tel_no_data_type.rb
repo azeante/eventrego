d900ac5bf74d803d5f1d4d1a5ee6c052cc760454
SZ <luminasce@gmail.com>
2016-09-05 00:21:11 +0800

    trial datatype change again for heroku

--- db/migrate/20160830103632_change_tel_no_data_type.rb
+++ db/migrate/20160830103632_change_tel_no_data_type.rb
@@ -1,5 +1,5 @@
 class ChangeTelNoDataType < ActiveRecord::Migration[5.0]
   def change
-    change_column :people, :telNo, :integer
+    change_column :people, :telNo, 'integer USING CAST("telNo" AS integer)'
   end
 end
