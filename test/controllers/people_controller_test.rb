f0f7b0f5c14dee2bf370c571017c325d3216efcc
SZ <luminasce@gmail.com>
2016-10-05 12:42:16 +0800

    fixed test should create person

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -19,16 +19,26 @@ class PeopleControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-# Should create person if doesn't exist
-  test "should create person" do
-    assert_difference('Person.count') do
+#should not create person if emailAddress exists
+# Should create person if emailAddress doesn't exist
+test "should create person" do
+  if Person.exists?(emailAddress: @person.emailAddress)
+    assert_difference('Person.count', 0) do
       post people_url, params: { person: { emailAddress: @person.emailAddress,
         firstName: @person.firstName, gender: @person.gender,
         ifSubscribed: @person.ifSubscribed,
         lastName: @person.lastName, telNo: @person.telNo } }
+      end
+    else
+      assert_difference('Person.count') do
+        post people_url, params: { person: { emailAddress: @person.emailAddress,
+          firstName: @person.firstName, gender: @person.gender,
+          ifSubscribed: @person.ifSubscribed,
+          lastName: @person.lastName, telNo: @person.telNo } }
+        end
+        assert_redirected_to person_url(Person.last)
+      end
     end
-    assert_redirected_to person_url(Person.last)
-  end
 
   test "should show person" do
     get person_url(@person)
